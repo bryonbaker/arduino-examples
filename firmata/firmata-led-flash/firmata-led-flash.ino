207394df6a159ede2bdfcdaa1f7825487ecf9d16
Bryon <bryonbakeraus@gmail.com>
2021-02-02 22:16:17 +1100

    Added the sample blink app This depends upon a python virtual environment that has pyfirmata installed. Refer to https://realpython.com/arduino-python/ for details

---
+++ firmata/firmata-led-flash/firmata-led-flash.ino
@@ -0,0 +1,16 @@
+#include <FirmataDefines.h>
+#include <FirmataConstants.h>
+#include <Boards.h>
+#include <Firmata.h>
+#include <FirmataMarshaller.h>
+#include <FirmataParser.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
