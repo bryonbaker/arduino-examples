d0481efc9a04870601d7316ee7ad70ecafb6a9ff
Bryon Baker <bryonbakeraus@gmail.com>
2021-04-29 22:31:25 +1000

    Added comments

--- firmata/firmata-led-flash/blink.py
+++ firmata/firmata-led-flash/blink.py
@@ -1,15 +1,22 @@
-from csinsc import *
-import pyfirmata
-import time
+from csinsc import *    # This library lets you use "goto" and "label"
+import pyfirmata        # This library lets you talk to the Arduino
+import time             # This library lets you pause the program
 
 board = pyfirmata.Arduino('/dev/cu.usbmodem144201')
 
-# while True:
 label .again
 
+# Turn the LED on
 board.digital[12].write(1)
+
+# Sleep for 200 milli-seconds
 time.sleep(0.2)
+
+# Turn the LED off
 board.digital[12].write(0)
+
+# Sleep for 1 second
 time.sleep(1)
 
+# Loop forever
 goto .again
